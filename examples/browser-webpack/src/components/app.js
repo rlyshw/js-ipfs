6882647259a5c1af9b92c89738705d90e048f4ad
rlyshw <rlyshw@gmail.com>
2018-09-18 22:01:49 -0400

    decided to use ipfs rooms

--- examples/browser-webpack/src/components/app.js
+++ examples/browser-webpack/src/components/app.js
@@ -2,11 +2,20 @@
 
 const React = require('react')
 const IPFS = require('ipfs')
-const OrbitDB = require('orbit-db')
+const Room = require('ipfs-pubsub-room')
+const randomWords = require("random-words")
 
 const ipfsOptions = {
+  repo:repo(),
   EXPERIMENTAL: {
     pubsub: true
+  },
+  config: {
+    Addresses: {
+      Swarm: [
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+      ]
+    }
   }
 }
 
@@ -17,89 +26,65 @@ class App extends React.Component {
     super(props)
     this.state = {
       id: null,
-      version: null,
-      protocol_version: null,
-      added_file_hash: null,
-      added_file_contents: null
+      roomVal: "",
+      joined: false
     }
+  }
+  join(self) {
+    // Create the IPFS node instance
+    let node = new IPFS(ipfsOptions)
+
+    node.once('ready', async () => {
+      node.id((err,info)=>{
+        if(err) console.log(err)
+
+        console.log('IPFS initialized with address '+info.id)
+
+        const room = Room(node, self.state.roomName)
+        room.on('peer joined', (peer) => {
+          console.log('Peer joined the room', peer)
+        })
+      
+        room.on('peer left', (peer) => {
+          console.log('Peer left...', peer)
+        })
+      
+        // now started to listen to room
+        room.on('subscribed', () => {
+          console.log('Now connected!')
+          self.setState({joined:true})
+        })
+      })
+    })
   }
   componentDidMount () {
-    const self = this
-    let node
-
-    create()
-
-    function create () {
-      // Create the IPFS node instance
-
-      //node = new IPFS({ repo: String(Math.random() + Date.now()) })
-      node = new IPFS(ipfsOptions)
-
-      node.once('ready', async () => {
-        console.log('IPFS node is ready')
-        const orbitdb = new OrbitDB(node)
-        const db = await orbitdb.log('hello')
-        db.events.on('ready',(dbname,heads)=>{
-          console.log(dbname,heads)
-        })
-        db.events.on('replicated',(addr)=>{
-          console.log(db.iterator({limit: -1}).collect())
-        })
-        await db.load()
-        const hash = await db.add('world')
-        console.log(hash)
-
-        const result = db.iterator({limit:-1}).collect()
-        console.log(JSON.stringify(result, null, 2))
-        ops()
-      })
-    }
-
-    function ops () {
-      node.id((err, res) => {
-        if (err) {
-          throw err
-        }
-        self.setState({
-          id: res.id,
-          version: res.agentVersion,
-          protocol_version: res.protocolVersion
-        })
-      })
-
-      node.files.add([Buffer.from(stringToUse)], (err, filesAdded) => {
-        if (err) { throw err }
-
-        const hash = filesAdded[0].hash
-        self.setState({added_file_hash: hash})
-
-        node.files.cat(hash, (err, data) => {
-          if (err) { throw err }
-          self.setState({added_file_contents: data})
-        })
-      })
-    }
   }
+
+  updateRoomName(e){
+    this.setState({
+      roomName:e
+    })
+  }
+
   render () {
     return (
       <div style={{textAlign: 'center'}}>
-        <h1>Everything is working!</h1>
-        <p>Your ID is <strong>{this.state.id}</strong></p>
-        <p>Your IPFS version is <strong>{this.state.version}</strong></p>
-        <p>Your IPFS protocol version is <strong>{this.state.protocol_version}</strong></p>
-        <hr />
-        <div>
-          Added a file! <br />
-          {this.state.added_file_hash}
-        </div>
-        <br />
-        <br />
-        <p>
-          Contents of this file: <br />
-          {this.state.added_file_contents}
-        </p>
+        <h1>IPFS RTC</h1>
+        {this.state.joined ? (<div>
+          <h2>Room name: {this.state.roomName}</h2>
+        </div>): (<div><button onClick={()=>{
+          this.updateRoomName(randomWords(5).join('-'));this.join(this)}
+          }>Create a room</button>
+        <br/>
+        <br/>
+        <input onChange={e=>this.updateRoomName(e.target.value)}></input><button onClick={()=>this.join(this)}>Join a room</button></div>)}
       </div>
     )
   }
 }
 module.exports = App
+
+
+function repo() {
+  return 'ipfs/pubsub/'+Math.random()
+}
